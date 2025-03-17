21644d23db96181e4406eb22661d8196d72e724b
wey <qaz237562@gmail.com>
2025-03-17 12:30:01 +0800

    0317上午js課程

---
+++ 0317jsdmo/abc.js
@@ -0,0 +1,18 @@
+// 變數 Variable
+// 宣告變數
+
+console.log("213");
+
+console.log("456");
+
+// var a = 1; // 宣告
+// a = 2; // 指定、給值、修改
+// let a = 1;  修正var的錯誤 var =重複宣告、變數提升=undefined
+// 建立期 要名字 初始化
+// 執行期 指定 給值 執行函數
+// let 不會初始化 var 初始化
+// const = 常數   不能被修改的變數-->不能重新給值 re-assign
+// a = 1 <--很可能變成全域變數bad 先宣告 var/let/const 後操作
+console.log(a);
+var a = 1;
+console.log(a);
