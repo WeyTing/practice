462b72a338fe6297bfaca5c54a3f2098184e6aa3
wey <qaz237562@gmail.com>
2025-04-02 12:30:36 +0800

    0402上午模組搭配框架 偷學tailwind

--- 0401/appp.js
+++ 0401/appp.js
@@ -1,12 +1,17 @@
 import { hey, a, hello } from "./ccc.js";
 import myname from "./ccc.js";
 import dayjs from "dayjs";
-hello();
-console.log(a);
-hey();
-console.log(hello);
-console.log(myname);
-myname();
-console.log(dayjs);
-hello();
-myname();
+//import {變數 as 新名字} from "axios" {可以改名遇到同名的變數時}
+import axios from "axios"; //import類似const再宣告一次
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start;
+
+const url = "https://api.5xcamp.us/api/v1/tenlong";
+const { data: books } = await axios.get(url);
+
+const title = books
+	.map((book) => book.title)
+	.forEach((book) => console.log(book));
+
+console.log(title);
