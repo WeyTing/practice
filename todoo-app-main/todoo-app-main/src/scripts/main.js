8855dfa90c11c069ee7364f7a0d24632fabd8628
wey <qaz237562@gmail.com>
2025-04-08 12:40:26 +0800

    0408 登入登出api

--- todoo-app-main/todoo-app-main/src/scripts/main.js
+++ todoo-app-main/todoo-app-main/src/scripts/main.js
@@ -1,15 +1,28 @@
 import axios from "axios"
 import Swal from "sweetalert2"
 
+const TOKEN_NAME = "private"
+
 const Main = () => ({
   showSection: "taskSection",
   email: "",
   nickname: "",
   password: "",
+  islogin: false,
   clearText() {
     this.email = ""
     this.nickname = ""
     this.password = ""
+  },
+  init() {
+    const token = localStorage.getItem(TOKEN_NAME)
+    if (token) {
+      this.islogin = true
+      this.showtaskInput()
+    }
+  },
+  showtaskInput() {
+    this.showSection = "taskSection"
   },
   showlogin() {
     this.showSection = "loginSection"
@@ -17,6 +30,43 @@ const Main = () => ({
   showsignUp() {
     this.showSection = "signUpSection"
   },
+  async logout() {
+    const url = "https://todoo.5xcamp.us/users/sign_out"
+    const token = localStorage.getItem(TOKEN_NAME)
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = token
+      const resp = await axios.delete(url)
+      console.log(resp)
+      this.showlogin()
+    }
+  },
+  async login() {
+    if (this.email != "" && this.password != "") {
+      const userData = {
+        user: {
+          email: this.email,
+          password: this.password,
+        },
+      }
+      try {
+        const resp = await axios.post("https://todoo.5xcamp.us/users/sign_in", userData)
+        console.log(resp.headers.authorization)
+        localStorage.setItem(TOKEN_NAME, resp.headers.authorization)
+        this.clearText()
+        this.islogin = true
+        this.showtaskInput()
+      } catch (err) {
+        console.log(err.response.data)
+        Swal.fire({
+          title: "OOPS",
+          html: err.response.data.message,
+          icon: "error",
+          confirmButtonText: "確認",
+        })
+      }
+    }
+  },
+
   async sign() {
     if (this.email != "" && this.nickname != "" && this.password != "") {
       const userData = {
@@ -38,24 +88,6 @@ const Main = () => ({
           icon: "error",
           confirmButtonText: "確認",
         })
-      }
-    }
-  },
-  async login() {
-    if (this.email != "" && this.password != "") {
-      const userData = {
-        user: {
-          email: this.email,
-          password: this.password,
-        },
-      }
-      try {
-        const resp = await axios.post("https://todoo.5xcamp.us/users/sign_in", userData)
-        console.log(resp.headers.authorization)
-        localStorage.setItem("private", resp.headers.authorization)
-        this.clearText()
-      } catch (err) {
-        console.log(err)
       }
     }
   },
