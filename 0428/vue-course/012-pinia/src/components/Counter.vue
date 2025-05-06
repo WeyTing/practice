160a2bd2a1ccc15ee92bd22dfaae0fc1d1a07f6c
wey <qaz237562@gmail.com>
2025-05-06 13:35:47 +0800

    0506 購物車輸入框調整

--- 0428/vue-course/012-pinia/src/components/Counter.vue
+++ 0428/vue-course/012-pinia/src/components/Counter.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { storeToRefs } from 'pinia'
-import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "@/stores/counter";
 
-const store = useCounterStore()
+const store = useCounterStore();
 // ⭕️ 正確用法
-const { count, doubleCount } = storeToRefs(store)
-const { increment } = store
+const { count, doubleCount } = storeToRefs(store); //ref 必須透過這種方式解構
+//const { increment } = store; //store.increment 方法
 </script>
 
 <template>
-  <div>
-    <p>Count: {{ count }}</p>
-    <p>Double: {{ doubleCount }}</p>
-    <button @click="increment">+1</button>
-  </div>
+	<div>
+		<p>Count: {{ count }}</p>
+		<p>Double: {{ doubleCount }}</p>
+		<button @click="increment">+1</button>
+	</div>
 </template>
